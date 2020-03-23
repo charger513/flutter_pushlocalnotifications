f201b8cb66540739b4dde8d129d50d9dc1dff840
Emmanuel Chargoy <charger513@hotmail.com>
2020-03-23 15:36:35 -0600

    Recibir notificación - onMessage, onLaunch y onResume

--- lib/src/providers/push_notifications_provider.dart
+++ lib/src/providers/push_notifications_provider.dart
@@ -12,6 +12,21 @@ class PushNotificationProvider {
 
       // eM97-X0OJrQ:APA91bH5bmXXID8jjbV7dFJ8RmPXcOVdm-EqKOXkhjBtYr_APuJF9JNlCpxp5UYiqpoOWngtxltsT5Duvb2_8NU2qfApfXcPey06JNMlVwBCqM1OvH-T-vZT3j-X8ekahvYd9UMiVqOf
     });
+
+    _firebaseMessaging.configure(
+      onMessage: (info) {
+        print('====== On Message ======');
+        print(info);
+      },
+      onLaunch: (info) {
+        print('====== On Launch ======');
+        print(info);
+      },
+      onResume: (info) {
+        print('====== On Resume ======');
+        print(info);
+      },
+    );
   }
 
 }
