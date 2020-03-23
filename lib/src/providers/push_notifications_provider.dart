51902979917b5cdf6cedd9c1f8d81d5b6957ceb4
Emmanuel Chargoy <charger513@hotmail.com>
2020-03-23 11:34:56 -0600

    Provider para controlar las notificaciones y FCM Token

---
+++ lib/src/providers/push_notifications_provider.dart
@@ -0,0 +1,17 @@
+import 'package:firebase_messaging/firebase_messaging.dart';
+
+class PushNotificationProvider {
+  final FirebaseMessaging _firebaseMessaging = FirebaseMessaging();
+
+  initNotifications() {
+    _firebaseMessaging.requestNotificationPermissions();
+
+    _firebaseMessaging.getToken().then((token) {
+      print('===== FCM Token =====');
+      print(token);
+
+      // eM97-X0OJrQ:APA91bH5bmXXID8jjbV7dFJ8RmPXcOVdm-EqKOXkhjBtYr_APuJF9JNlCpxp5UYiqpoOWngtxltsT5Duvb2_8NU2qfApfXcPey06JNMlVwBCqM1OvH-T-vZT3j-X8ekahvYd9UMiVqOf
+    });
+  }
+
+}
